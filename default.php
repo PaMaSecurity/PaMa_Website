106ad075fb6342f3de7e7f0c6cecb28f781d46df
Elie RUGGIERO <elie.ruggiero25@gmail.com>
2023-10-02 21:52:46 +0200

    init

---
+++ default.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <title>PaMa</title>
+        <link rel="icon" type="image/x-icon" href="icon.png">
+        <meta charset="utf-8">
+    </head>
+    <body>
+        <h1>PaMa, à venir prochainement</h1>
+    </body>
+</html>
