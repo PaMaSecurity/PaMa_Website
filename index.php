7207e298e592e977680a926d68b301613001ce7b
Elie RUGGIERO <elie.ruggiero25@gmail.com>
2023-10-03 19:09:33 +0200

    separation

--- index.php
+++ index.php
@@ -1,12 +1,3 @@
-<!DOCTYPE html>
-<html lang="fr">
-    <head>
-        <title>PaMa</title>
-        <link rel="icon" type="image/x-icon" href="icon-colour.svg">
-        <meta charset="utf-8">
-    </head>
-    <body>
-        <h1>PaMa, à venir prochainement</h1>
-        <a target="_blank" href="https://www.instagram.com/pama_security/">instagram</a>
-    </body>
-</html>
+<?php
+include("/home.html")
+?>
